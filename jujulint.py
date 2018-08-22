a829e2724479117f68ade1c0b33052f4d86ca408
Stuart Bishop <stuart.bishop@canonical.com>
2018-08-23 03:02:21 +0700

    Attach GPLv3 license information

--- jujulint.py
+++ jujulint.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2018 James Troup <james.troup@canonical.com>
+# This file is part of juju-lint, a tool for validating that Juju
+# deloyments meet configurable site policies.
+#
+# Copyright 2018 Canonical Limited.
+# License granted by Canonical Limited.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import json
 import logging
