ab75af0aaa1e7ce79464ceb1ecbd742b6e3ceb9b
James Hebden <james@ec0.io>
2020-05-12 17:05:44 +1000

    Updated build tooling

--- setup.py
+++ setup.py
@@ -26,26 +26,24 @@ warnings.simplefilter("ignore", UserWarning)  # Older pips complain about newer 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open("debian/changelog", "r") as fh:
-    version = re.search(r'\((.*)\)', fh.readline()).group(1)
-
 setuptools.setup(
     name="jujulint",
-    version=version,
+    use_scm_version=True,
     author="Canonical",
     author_email="juju@lists.ubuntu.com",
     description="Linter for Juju models to compare deployments with configurable policy",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://launchpad.net/juju-lint",
-    classifiers=(
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Development Status :: 2 - Beta",
         "Environment :: Plugins",
-        "Intended Audience :: System Administrators"),
-    python_requires='>=3.4',
+        "Intended Audience :: System Administrators",
+    ],
+    python_requires=">=3.4",
     py_modules=["jujulint"],
-    entry_points={
-        'console_scripts': [
-            'juju-lint=jujulint.cli:main']})
+    entry_points={"console_scripts": ["juju-lint=jujulint.cli:main"]},
+    setup_requires=['setuptools_scm'],
+)
